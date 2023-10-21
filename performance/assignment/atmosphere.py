ee37a0503c68d98d4024e44481ba626401496e08
pstet <123316024+pcs03@users.noreply.github.com>
2023-10-21 17:18:44 +0200

    Clean the code for the landing performance assignment

--- performance/assignment/atmosphere.py
+++ performance/assignment/atmosphere.py
@@ -1,35 +1,35 @@
 from constants import Atmosphere, Earth
 
 
-def get_temp_at_height(height_m: float) -> float:
+def get_temp(height: float) -> float:
     """
-    Returns the temperature at a height in ft [°K]
+    Returns the temperature at a height in meter [°K]
     """
 
-    temp_at_height_k = Atmosphere.Sealevel.temp - Atmosphere.lapse_rate * height_m
+    temp_at_height_k = Atmosphere.Sealevel.temp - Atmosphere.lapse_rate * height
 
     return temp_at_height_k
 
 
-def get_pressure_at_temp(temp_k: float) -> float:
+def get_pressure(temp: float) -> float:
     """
-    Returns the pressure at a given atmospheric temperature [Pa]
+    Returns the pressure at a given atmospheric temperature in Kelvin [Pa]
     """
 
-    pressure_at_temp = Atmosphere.Sealevel.pressure * (temp_k / Atmosphere.Sealevel.temp) ** (
+    pressure_at_temp = Atmosphere.Sealevel.pressure * (temp / Atmosphere.Sealevel.temp) ** (
         Earth.gravity / (Atmosphere.lapse_rate * Atmosphere.r_air)
     )
 
     return pressure_at_temp
 
 
-def get_density_at_height(height_m: float) -> float:
+def get_density(height: float) -> float:
     """
-    Returns the pressure at a given height in ft [kg / m^3]
+    Returns the pressure at a given height in meter [kg / m^3]
     """
 
-    temp_at_height = get_temp_at_height(height_m)
-    pressure_at_height = get_pressure_at_temp(temp_at_height)
+    temp_at_height = get_temp(height)
+    pressure_at_height = get_pressure(temp_at_height)
     density_at_height = pressure_at_height / (Atmosphere.r_air * temp_at_height)
 
     return density_at_height
