a98d00a43622a58f99bde113567f69b42ca15503
pstet <123316024+pcs03@users.noreply.github.com>
2023-12-13 19:56:15 +0100

    Add optimization 3-2

--- optimization/ws3/Workshop_3_2_students.py
+++ optimization/ws3/Workshop_3_2_students.py
@@ -1,4 +1,3 @@
-
 """
 V0: Creation
 V1: New exercise: Recursive Function 
@@ -17,40 +16,40 @@ Please notice that the provided files are given in .xls (DATA on DLO)
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import vinc
 import time
+import os
 
 ##============================================================================
 ## Exercise 1
 ## Using the command read_csv and the help information (which can be found in
 ## https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html)
-## load the file flight_csv.csv. 
+## load the file flight_csv.csv.
 ## use print(data_frame.head()) to visualize your data
 ##============================================================================
 
-
+df = pd.read_csv(os.path.abspath("./optimization/ws3/flight_csv.csv"))
+print(df.columns)
 
 
 ##============================================================================
 ## Exercise 2
 ## Observe your variables in your data frame. Giving the goal of your assignment.
-## Is there any variable (column) that you can drop (erase/remove) to make 
+## Is there any variable (column) that you can drop (erase/remove) to make
 ## your dataframe a bit smaller? If so, drop it.
 ## It is always a good practicer to NEVER alter your original dataframe.
 ## For this reason, make a copy of your data frame and read how to drop columns
 ## https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.drop.html
 ## For your assignment you should analyse what variables from your data frame
-## are useful. 
+## are useful.
 ##============================================================================
-
 
 
 ##============================================================================
 ## Exercise 3
 ## For the rest of excercises, just leave the columns [3d Latitude],
 ## [3d Latitude]','[3d Longitude]','[3d Altitude Ft].
-## Names are long and cumbersome. Once you have selected those values from 
-## the dataframe, rename them to LAT, LON, and ALT. 
+## Names are long and cumbersome. Once you have selected those values from
+## the dataframe, rename them to LAT, LON, and ALT.
 ## Hint: df.rename()
 ## Print the first 10 values and the last 10 values. head() and tail() could
 ## be useful
@@ -58,26 +57,34 @@ import time
 ##============================================================================
 
 
-
-
+df = df.loc[
+    :,
+    [
+        "[3d Latitude]",
+        "[3d Longitude]",
+        "[3d Altitude Ft]",
+    ],
+]
+df = df.rename(columns={"[3d Latitude]": "lat", "[3d Longitude]": "lon", "[3d Altitude Ft]": "alt"})
+print(df.head())
+print(df.describe())
 
 ##============================================================================
 ## Exercise 4
-## You may have noticed that there are values such as (lat,lon) = (0,0). This is 
+## You may have noticed that there are values such as (lat,lon) = (0,0). This is
 ## evidently not correct. It might be because the navigation tool was not yet
 ## activated. Go ahead and remove those values from your dataframe.
-## use .describe() and verify the max and min LAT,LON values. 
+## use .describe() and verify the max and min LAT,LON values.
 ## Do they make sense? What about the Altitude at Schiphol? Is the max altitude
 ## during flight realistic?
 ## When working with data sets it is always important to ask these questions.
-## If you do not know, it is time to ask an expert about typical values.  
+## If you do not know, it is time to ask an expert about typical values.
 ## You can as well use the function .drop_duplicates() after to remove the
 ## The duplicated vales
 ##============================================================================
 
-
-
-
+df = df[(df["lat"] > 0) & (df['lon'] > 0)]
+print(df.describe())
 
 ##============================================================================
 ## Exercise 5
@@ -86,21 +93,20 @@ import time
 ## What do you observe? (Do not read Exericse 6 just yet. Think about it)
 ##============================================================================
 
-#fig, ax = plt.subplots()
-#ax.plot(df.ALT[:])
-#ax.set(xlabel='Waypoint #', ylabel='Altitude (ft)',
+# fig, ax = plt.subplots()
+# ax.plot(df.ALT[:])
+# ax.set(xlabel='Waypoint #', ylabel='Altitude (ft)',
 #        title='Flight Profile')
-#fig.savefig("test.png")   # Only if you want to save the plot on y our HDD. 
-#ax.grid()
-#plt.show()
-
+# fig.savefig("test.png")   # Only if you want to save the plot on y our HDD.
+# ax.grid()
+# plt.show()
 
 
 ##============================================================================
 ## Exercise 6
-## You have tought about it? 
+## You have tought about it?
 ## Then you have noticed that the first and the last
-## waypoints are always at the same altitude . Why is that? Should you remove them? 
+## waypoints are always at the same altitude . Why is that? Should you remove them?
 ## Just for fun let's remove them and plot the data again
 ## You might want to explore PANDASERIES.to_frame(), Transpose x.T
 ## and pd.conct(....).reset_index(drop = True) or any other way you might know
@@ -115,52 +121,45 @@ import time
 ##============================================================================
 
 
-
-
 ##============================================================================
 ## Exercise 8
-## It looks ugly now, does not it? These are real flights, it is hard to keep 
-## exact altitude. That is also the reason why there is vertical separation, 
+## It looks ugly now, does not it? These are real flights, it is hard to keep
+## exact altitude. That is also the reason why there is vertical separation,
 ## to account for this errors. Extend the plot scale to reduce this effect.
 ## Hint: ax.set_ylim()
 ##============================================================================
 
 
-
-
 ##============================================================================
 ## Exercise 9
-## Use the complete flight data (Ex 6) set and using vincenty, compute the total 
+## Use the complete flight data (Ex 6) set and using vincenty, compute the total
 ## flight distance taking all points into account. Measure the computation time
-## Create a list where you can also store the distance between waypoints. 
+## Create a list where you can also store the distance between waypoints.
 ##============================================================================
-
 
 
 ##============================================================================
 ## Exercise 10
-## you might encouter that your waypoints are too close togetheter. 
+## you might encouter that your waypoints are too close togetheter.
 ## Select 10 waypoints (Plus the initial two for a total of 12)
 ## Compute the distance between those waypoints. They should be the same.
 ## Measure the computation time
 ##============================================================================
 
-#begin_ = time.perf_counter()
+# begin_ = time.perf_counter()
 # YOUR CODE
-#end_ = time.perf_counter()
-#total_time = end_ - begin_
+# end_ = time.perf_counter()
+# total_time = end_ - begin_
 
 ##============================================================================
 ## Exercise 11
 ## load the dataset named flightaware.txt avaiable in DLO
-## You guessed it. I just copy and pasted this data from flightaware in a 
-## .txt file. 
+## You guessed it. I just copy and pasted this data from flightaware in a
+## .txt file.
 ## Load this file to python
-## Clean it 
+## Clean it
 ## Plot the complete flight
 ##============================================================================
-
-
 
 
 ##============================================================================
@@ -169,23 +168,24 @@ import time
 ## Example:
 ##============================================================================
 def down_to_number(initial, n):
-     print(initial)
-     if initial == n:
-         return             # Terminate recursion (Let's get out of here)
-     else:
-         initial -= 1
-         down_to_number(initial, n)   # Recursive call
-         
+    print(initial)
+    if initial == n:
+        return  # Terminate recursion (Let's get out of here)
+    else:
+        initial -= 1
+        down_to_number(initial, n)  # Recursive call
+
+
 # Observe the behaviour closely, pat attention to recursive call
-down_to_number(initial = 10, n = 1)
+down_to_number(initial=10, n=1)
 
 # Do you understand what it is happening?
 
 ##============================================================================
 ## Exercise 13
 ## Recursive functions: Function that calls itself
-## Now try to think how to solve the typical example 
-## Find the factorial of a given number n by using a recursive function. 
+## Now try to think how to solve the typical example
+## Find the factorial of a given number n by using a recursive function.
 ## n! = n × (n – 1) × (n – 2) × … × 1
 ##============================================================================
 
